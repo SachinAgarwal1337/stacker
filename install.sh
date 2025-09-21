c55d4f8aac85a161712e8c6be9a2eeb5be64e6e2
Sachin Agarwal <sachinkumaragarwal05@gmail.com>
2025-09-21 15:02:58 +0530

    fix

--- install.sh
+++ install.sh
@@ -1,21 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ -f /etc/lsb-release ]]; then
-    cat /etc/lsb-release | grep "[20.04|22.04]" > /dev/null
-    if ![[ $? -eq 0 ]]; then
-        # if [[ $(uname -m) != "x86_64" ]]; then
-        #     echo "Stacker only supports x86_64 architecture."
-        #     exit 1
-        # fi
-    # else
-        echo "Stacker only supports Ubuntu."
-        exit 1
-    fi
-else
-    echo "Stacker only supports Ubuntu."
-    exit 1
-fi
-
 echo "Installing Stacker.."
 
 sudo curl -sSL https://raw.githubusercontent.com/SachinAgarwal1337/stacker/master/stacker.sh > stacker
