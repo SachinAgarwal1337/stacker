d766eb6b37a2a133f564a4bbd19ef050934e843d
Sachin Agarwal <sachinkumaragarwal05@gmail.com>
2025-09-21 15:01:36 +0530

    fix

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-if [[ -f /etc/lsb-release ]]; then
-    cat /etc/lsb-release | grep "[20.04|22.04]" > /dev/null
-    if ![[ $? -eq 0 ]]; then
-        # if [[ $(uname -m) != "x86_64" ]]; then
-        #     echo "Stacker only supports x86_64 architecture."
-        #     exit 1
-        # fi
-    # else
-        echo "Stacker only supports Ubuntu."
-        exit 1
-    fi
-else
-    echo "Stacker only supports Ubuntu."
-    exit 1
-fi
+# if [[ -f /etc/lsb-release ]]; then
+#     cat /etc/lsb-release | grep "[20.04|22.04]" > /dev/null
+#     if [[ $? -eq 0 ]]; then
+#         # if [[ $(uname -m) != "x86_64" ]]; then
+#         #     echo "Stacker only supports x86_64 architecture."
+#         #     exit 1
+#         # fi
+#     # else
+#         echo "Stacker only supports Ubuntu."
+#         exit 1
+#     fi
+# else
+#     echo "Stacker only supports Ubuntu."
+#     exit 1
+# fi
 
 echo "Installing Stacker.."
 
